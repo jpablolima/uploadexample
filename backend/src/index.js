b44d16185f9c0c89e310deb763f4c097ac97d87d
João Pablo <jp.pablolima@gmail.com>
2019-01-27 16:38:28 -0200

    backend concluido

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,29 @@
-require ("dontenv").config();
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
-/*
-    Data Base
-*/
-
+/**
+ * Database setup
+ */
 mongoose.connect(
-    "mongodb://localhost:27017/upload",
-    {
-        useNewUrlParser: true
-    }
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true
+  }
 );
 
-
 app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 
 app.use(require("./routes"));
 
