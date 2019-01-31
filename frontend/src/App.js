62bd7634f3058bb59de10c164e6a97f354e09f77
João Pablo <jp.pablolima@gmail.com>
2019-01-31 19:24:38 -0200

    componentes FIle List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import GlobalStyle from "./styles/global";
 import { Container, Content   } from "./styles";
 import Upload from "./components/Upload";
-
+import FileList from "./components/FileList";
 
 class App extends Component {
   render() {
@@ -11,6 +11,7 @@ class App extends Component {
       <Container>
         <Content> 
           <Upload/>
+          <FileList/>
          </Content>
         <GlobalStyle/>
       </Container>
