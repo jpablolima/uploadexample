f7c301ce5eda35d3f62b7770cba9b89a0c6d43bd
João Pablo <jp.pablolima@gmail.com>
2019-02-01 20:21:50 -0200

    icon URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -58,8 +58,19 @@ class App extends Component {
             progress,
           })
         }
-      })        
-      }
+      }).then(response  => {
+       this.updateFile(uploadedFile.id, {
+         uploaded:true,
+         id:response.data._id,
+         url: response.data.url
+       });
+      }).catch(() => {
+        this.updateFile(uploadedFile.id, {
+          error:true
+        });
+
+      });        
+      };
 
 
 
